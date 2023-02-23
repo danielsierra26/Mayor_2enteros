7932208c146dae46d06b2f4714b63aeb9ee274da
danielsierra26 <danielsierra@colegioguanenta.edu.co>
2023-02-23 16:22:25 -0500

    Se agrego el diagrama de flujo, el progama y el README

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Mayor_2enteros
-Programa para verificar cuál de dos números  entero es el mayor
+Programa para verificar cuál de dos números entero es el mayor
 
 ## Diagrama de flujo
 ![diagrama de flujo](diagrama.png "diagrama de flujo")
--- mayor_dosenteros.py
+++ mayor_dosenteros.py
@@ -1,1 +1,19 @@
-Programa para verificar cuál de dos números entero es el mayor
+# PROGRAMA PARA VERIFICAR CUÁL DE DOS NÚMEROS ENTEROS ES EL MAYOR 
+
+
+#input
+x = int(input("INGRESE EL VALOR DE X: "))
+y = int(input("INGRESE EL VALOR DE Y: "))
+
+#processing
+
+if x == y: 
+    # output
+    print("LOS NÚMEROS SON IGUALES...")
+else:
+    if x > y:
+        mayor = x
+    else:
+        mayor = y 
+        # output
+        print("EL MAYOR ENTRE: " + str(x) + " y " + str(y) + " ES= " + str(mayor))
